41295d26b3b3e17645b8df3d499568e8c36365a0
codewithsaidul <codewithsaidul@gmail.com>
2025-05-16 21:11:14 +0600

    learning add a new column on existing table with alter keyword

--- alter.sql
+++ alter.sql
@@ -1,2 +1,7 @@
 -- Active: 1747406338289@@127.0.0.1@5432@ph
-SELECT * FROM student;
+SELECT * FROM enrollment;
+
+ALTER TABLE enrollment
+    ADD COLUMN courseName VARCHAR(50) NOT NULL;
+
+INSERT INTO enrollment VALUES (5, 'saidul', 'Saidul Islam', 'exam@gmail.com', 23, 'TypeScript Technocrat');
